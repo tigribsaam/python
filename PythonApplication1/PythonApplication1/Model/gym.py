ebfa1099d50f1ddbabd1a7c27e5d2da2b73da8f3
Birgit <birgitmaas@hotmail.com>
2020-12-15 17:48:31 +0100

    gym

--- PythonApplication1/PythonApplication1/Model/gym.py
+++ PythonApplication1/PythonApplication1/Model/gym.py
@@ -1,1 +1,25 @@
+class Gym(object):
+    def __init__(self, nm):
+        self.__name = nm
+        self.__week_schedule = []
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, nm):
+        self.__name = nm
+
+    def find_day(self, date):
+        wanted_day = None
+
+        for  d in self.__week_schedule:
+            if d.get_date() == date:
+                wanted_date = d
+                break
+
+        if wanted_date == None:
+            wanted_date = Day(date)
+            self.__week_schedule.append(wanted_date)
+
+        return wanted_date
 
--- PythonApplication1/PythonApplication1/main.py
+++ PythonApplication1/PythonApplication1/main.py
@@ -1,30 +1,71 @@
-from Model.person import Person
-from Model.member import Member
-from Model.employee import Employee
-from Model.traininggoal import TrainingGoal
-from Model.gymclass import GymClass
-from Model.day import Day
-from datetime import datetime
-import time
+from tkinter import *
 
 
-d1 = Day(datetime.today())
+root = Tk()
 
-tg = TrainingGoal('Arnold Schwarzenegger wannabe', "elke dag sporten")
+root.title('rbgsjrfl')
+root.geometry('700x500')
 
-m1 = Member("m1", "m1@email.com")
 
-m2 = Member("m2", "m2@email.com", tg)
+a = Label(root,text='Hello, world!')
+a.grid()
+lbl = Label(root, text = "Are you a Geek?") 
+lbl.grid() 
 
-e1 = Employee('e1', 'e1@email.com', 'instructor')
+def clicked(): 
+    lbl.configure(text = "I just got clicked") 
+  
+# button widget with red color text 
+# inside 
+btn = Button(root, text = "Click me" , 
+             fg = "red", command=clicked) 
+  
+btn.grid(column=1, row=0) 
 
-yoga = d1.find_class_by_time('09:00')
+root.mainloop()
 
-yoga.add_members(m1)
-yoga.add_members(m2)
-yoga.add_members(m1)
-yoga.set_instructor(e1)
-print(d1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#from Model.person import Person
+#from Model.member import Member
+#from Model.employee import Employee
+#from Model.traininggoal import TrainingGoal
+#from Model.gymclass import GymClass
+#from Model.day import Day
+#from datetime import datetime
+#import time
+
+
+#d1 = Day(datetime.today())
+
+#tg = TrainingGoal('Arnold Schwarzenegger wannabe', "elke dag sporten")
+
+#m1 = Member("m1", "m1@email.com")
+
+#m2 = Member("m2", "m2@email.com", tg)
+
+#e1 = Employee('e1', 'e1@email.com', 'instructor')
+
+#yoga = d1.find_class_by_time('09:00')
+
+#yoga.add_members(m1)
+#yoga.add_members(m2)
+#yoga.add_members(m1)
+#yoga.set_instructor(e1)
+#print(d1)
 
 
 
